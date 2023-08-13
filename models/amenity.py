00c41bcec8970ecfae911136b2243b8bcdbd1997
Caesar254 <augustineodu9@gmail.com>
2023-08-13 10:40:43 -0700

    Airbnb

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''class inherent of BaseModel'''
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    '''class amenity'''
+    """Represent an amenity class.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
 
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
 
